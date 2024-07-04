0113ca533a710900134077726f76b028dc783852
gjaegyun <jaegyungim06@gmail.com>
2024-07-04 18:00:58 +0900

    Add style.ts

---
+++ apps/client/src/components/SelectShoppingCart/style.ts
@@ -0,0 +1,65 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1.75rem;
+  width: 100%;
+  background: ${({ theme }) => theme.color.background}; //문제 시 삭제
+`;
+
+export const SelectAllContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1.5rem;
+`;
+
+export const SelectAllBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.25rem;
+`;
+
+export const SelectButtonProvider = styled.button`
+  display: flex;
+  width: fit-content;
+  height: fit-content;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SelectAllText = styled.p`
+  ${({ theme }) => theme.typo.bodyLarge};
+  color: ${({ theme }) => theme.color.white};
+`;
+
+export const GrayBar = styled.div`
+  width: 0.0625rem;
+  height: 1.25rem;
+  background: ${({ theme }) => theme.color.grey[800]};
+`;
+
+export const DeleteButton = styled.button`
+  ${({ theme }) => theme.typo.bodyLarge};
+  color: ${({ theme }) => theme.color.error};
+`;
+
+export const MapGrayBar = styled.div`
+  width: 100%;
+  height: 0.0625rem;
+  background: ${({ theme }) => theme.color.grey[800]};
+`;
+
+export const ProductBox = styled.div<{ isRemoving: boolean }>`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 2.5rem;
+  transition: opacity 0.3s ease-in, max-height 0.3s ease-in-out, margin 0.3s ease-in-out;
+  opacity: ${({ isRemoving }) => (isRemoving ? 0 : 1)};
+  overflow: hidden;
+`;
