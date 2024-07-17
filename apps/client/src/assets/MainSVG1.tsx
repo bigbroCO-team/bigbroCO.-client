13f59a7bbe01582e2d2a993d637d9f8a74a15c75
gaoooon <gaon9749@gmail.com>
2024-07-17 15:36:28 +0900

    Add MainSVG1, 2, 3, 4

---
+++ apps/client/src/assets/MainSVG1.tsx
@@ -0,0 +1,16 @@
+const MainSVG1 = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='25.3125rem'
+    height='5.625rem'
+    viewBox='0 0 405 90'
+    fill='none'
+  >
+    <path
+      d='M0 5H360C382.091 5 400 22.9086 400 45V45C400 67.0914 382.091 85 360 85H0'
+      stroke='white'
+      strokeWidth='10'
+    />
+  </svg>
+);
+export default MainSVG1;
