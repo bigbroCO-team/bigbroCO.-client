e27a304c2929b222b764baa3fffec6be2057f48b
gjaegyun <jaegyungim06@gmail.com>
2024-07-18 16:48:06 +0900

    Add component import

--- apps/client/src/components/index.ts
+++ apps/client/src/components/index.ts
@@ -1,5 +1,6 @@
+export * from './Input';
+export * from './Modal';
 export { default as CheckProduct } from './CheckProduct';
 export { default as ImgDisplay } from './ImgDisplay';
 export { default as AddressInfo } from './AddressInfo';
-export * from './Input';
-export * from './Modal';
+export { default as SelectShoppingCart } from './SelectShoppingCart';
