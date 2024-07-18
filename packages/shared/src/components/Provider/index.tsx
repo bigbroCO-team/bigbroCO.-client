3bec928796965c047e7a12eeb66ed9a8b56ef8d6
gaoooon <gaon9749@gmail.com>
2024-07-18 22:20:31 +0900

    Add useClient

--- packages/shared/src/components/Provider/index.tsx
+++ packages/shared/src/components/Provider/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { ThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
