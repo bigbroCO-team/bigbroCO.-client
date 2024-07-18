0c91918bd12d99fc240e835c82ecb7350f36741f
gaoooon <gaon9749@gmail.com>
2024-07-18 23:28:39 +0900

    Delete SVG

--- packages/shared/src/components/Header/index.tsx
+++ packages/shared/src/components/Header/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import * as S from './style';
-import * as I from 'shared/assets';
 import type { HeaderType, HeaderPositionType } from 'shared/types';
 import { useSelectedLayoutSegment } from 'next/navigation';
 
@@ -15,27 +14,15 @@ const Header: React.FC<Props> = ({ type, position = 'fixed' }) => {
 
   return (
     <S.Container type={type} position={position} segment={segment}>
-      <S.LogoWrapper href='/'>
-        <I.BIGBROCOMPANYLogo />
-      </S.LogoWrapper>
+      <S.BIGBROCOMPANY href='/'>BIGBRO COMPANY</S.BIGBROCOMPANY>
       <S.NavContainer>
         {type !== 'admin' && (
           <>
-            <S.LogoWrapper href='/CBWAS'>
-              <I.CBWASLogo />
-            </S.LogoWrapper>
-            <S.LogoWrapper href='/S.C.B'>
-              <I.SCBLogo />
-            </S.LogoWrapper>
-            <S.LogoWrapper href='/BIGBRO'>
-              <I.BIGBROLogo />
-            </S.LogoWrapper>
-            <S.LogoWrapper href='/GONGNEWGI'>
-              <I.GONGNEWGILogo />
-            </S.LogoWrapper>
-            <S.LogoWrapper href='/SCULFEE'>
-              <I.SCULFEELogo />
-            </S.LogoWrapper>
+            <S.LogoWrapper href='/CBWAS'>CBWAS</S.LogoWrapper>
+            <S.LogoWrapper href='/S.C.B'>S.C.B</S.LogoWrapper>
+            <S.LogoWrapper href='/BIGBRO'>BIGBRO</S.LogoWrapper>
+            <S.LogoWrapper href='/GONGNEWGI'>GONGNEWGI</S.LogoWrapper>
+            <S.LogoWrapper href='/SCULFEE'>SCULFEE</S.LogoWrapper>
           </>
         )}
         {type === 'admin' && (
@@ -45,14 +32,12 @@ const Header: React.FC<Props> = ({ type, position = 'fixed' }) => {
           </>
         )}
       </S.NavContainer>
-      <S.LoginButton href='/login'>
-        <I.LoginText />
-      </S.LoginButton>
+      <S.LoginButton href='/login'>Login</S.LoginButton>
       {/* <S.LogoutButton href='/logout'>
-        <I.LogoutText />
+        Logout
       </S.LogoutButton>
       <S.MyPageButton href='/'>
-        <I.MyPageText />
+        my page
       </S.MyPageButton> */}
     </S.Container>
   );
