62e8d21df9774da16c8031caef1a828da98d7bb2
gaoooon <gaon9749@gmail.com>
2024-07-24 16:31:27 +0900

    Update Provider component

--- packages/shared/src/components/Provider/index.tsx
+++ packages/shared/src/components/Provider/index.tsx
@@ -1,34 +1,27 @@
 'use client';
 
 import { ThemeProvider } from '@emotion/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
+import { ToastContainer } from 'react-toastify';
 import { GlobalStyle, theme } from 'shared/styles';
+import 'react-toastify/dist/ReactToastify.css';
+import { bigbroQueryClient } from 'shared/utils';
 
 interface Props {
   readonly children: React.ReactNode;
 }
 
 const Provider: React.FC<Props> = ({ children }) => {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            retry: false,
-            staleTime: 300000, // 5분
-            gcTime: 300000,
-          },
-        },
-      })
-  );
+  const [queryClient] = useState(() => bigbroQueryClient);
 
   return (
     <ThemeProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools />
         <GlobalStyle />
+        <ToastContainer theme='dark' />
         {children}
       </QueryClientProvider>
     </ThemeProvider>
