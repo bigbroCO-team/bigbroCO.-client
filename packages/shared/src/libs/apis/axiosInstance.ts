e5040114ee4c4ac683034bca5753a6fc48f5f0e7
gaoooon <gaon9749@gmail.com>
2024-07-24 16:31:08 +0900

    Update axiosInstance

--- packages/shared/src/libs/apis/axiosInstance.ts
+++ packages/shared/src/libs/apis/axiosInstance.ts
@@ -1,19 +1,36 @@
 import axios from 'axios';
+import { checkUrl } from 'shared/utils';
 
 export const axiosInstance = axios.create({
   baseURL: '/api/v1',
   withCredentials: true,
 });
 
+axiosInstance.interceptors.request.use((config) => {
+  if (config.url && checkUrl(config.url)) {
+    console.log(config);
+
+    return config;
+  }
+
+  const accessToken = localStorage.getItem('accessToken');
+  if (accessToken) config.headers.Authorization = `Bearer ${accessToken}`;
+  // todo refresh 로직 추가
+
+  return config;
+});
+
 axiosInstance.interceptors.response.use(
   (response) => {
-    if (response.status >= 200 && response.status <= 300) {
+    if (response.status === 200) {
       return response.data;
     }
 
     return Promise.reject(response.data);
   },
   (error) => {
+    if (error.response.data) return error.response.data;
+
     return Promise.reject(error);
   }
 );
