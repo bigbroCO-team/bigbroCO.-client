a7a4954cf0d6665105ff51b88418bf049668bc8d
pki071120 <ddametyy@gmail.com>
2024-07-25 13:04:43 +0900

    Add LeftArrowSVG

---
+++ apps/client/src/assets/LeftArrow.tsx
@@ -0,0 +1,32 @@
+const LeftArrow = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='8'
+      height='15'
+      viewBox='0 0 8 15'
+      fill='none'
+    >
+      <g clip-path='url(#clip0_257_9128)'>
+        <path
+          d='M7 13.5L1.70711 8.20711C1.31658 7.81658 1.31658 7.18342 1.70711 6.79289L7 1.5'
+          stroke='#8C8C8C'
+          stroke-width='2'
+          stroke-linecap='round'
+        />
+      </g>
+      <defs>
+        <clipPath id='clip0_257_9128'>
+          <rect
+            width='8'
+            height='14'
+            fill='white'
+            transform='translate(8 14.5) rotate(-180)'
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default LeftArrow;
--- apps/client/src/assets/index.ts
+++ apps/client/src/assets/index.ts
@@ -4,5 +4,6 @@ export { default as CheckBox } from './CheckBox';
 export { default as MinusIcon } from './MinusIcon';
 export { default as PlusIcon } from './PlusIcon';
 export { default as XIcon } from './XIcon';
+export { default as LeftArrow } from './LeftArrow';
 export * from './mainPageSVG';
 export * from './loginPageSVG';
