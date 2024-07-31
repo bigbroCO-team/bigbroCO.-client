13b39d5b7411653a87b9a119dbc8d834bc8ec1aa
gaoooon <gaon9749@gmail.com>
2024-07-31 22:53:28 +0900

    Connect API

--- apps/client/src/layouts/productList/index.tsx
+++ apps/client/src/layouts/productList/index.tsx
@@ -3,14 +3,20 @@
 import { BrandType } from 'shared/types';
 import * as S from './style';
 import { backgroundImg } from 'client/public';
-import { Header } from 'shared';
+import { Header, useGetProductList } from 'shared';
 import { ProductItem } from 'client/components';
+import { minutesToMs } from 'client/utils';
 
 interface ProuctListLayoutProps {
   brand: BrandType;
 }
 
 const PorductListLayout: React.FC<ProuctListLayoutProps> = ({ brand }) => {
+  const { data } = useGetProductList(brand, {
+    staleTime: minutesToMs(10),
+    gcTime: minutesToMs(10),
+  });
+
   return (
     <S.Container>
       <Header type='client_img' />
@@ -18,15 +24,17 @@ const PorductListLayout: React.FC<ProuctListLayoutProps> = ({ brand }) => {
         <S.InnerContainer>
           <S.Title>{brand}</S.Title>
           <S.ProductContainer>
-            {[1, 2, 3, 4, 5, 6, 7].map((i) => (
-              <ProductItem
-                key={i}
-                productImgUrl='test'
-                discountPercent='10'
-                price='9000'
-                productName='레전드 티셔츠'
-              />
-            ))}
+            {data &&
+              data.map(({ id, name, price, images, discount }) => (
+                <ProductItem
+                  id={id}
+                  key={id}
+                  productImgUrl={images[0].url}
+                  discountPercent={discount}
+                  price={price}
+                  name={name}
+                />
+              ))}
           </S.ProductContainer>
         </S.InnerContainer>
       </S.ShadowBox>
