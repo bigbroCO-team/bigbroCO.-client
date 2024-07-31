ef518c87ad502638907c3a05de23fefb9b9362e4
gaoooon <gaon9749@gmail.com>
2024-07-31 22:13:18 +0900

    Update regex

--- packages/shared/src/utils/checkUrl.ts
+++ packages/shared/src/utils/checkUrl.ts
@@ -1,11 +1,11 @@
 /**
- * '/auth/login', '/auth/signup', '/product/', '/product/숫자' 해당 형식의 문자열이 맞는지 확인하는 함수입니다
+ * '/auth/login', '/auth/signup', '/product + 아무 문자', 해당 형식의 문자열이 맞는지 확인하는 함수입니다
  *
  * @returns boolean
  */
 
 const checkUrl = (url: string) => {
-  const notUseTokenUrlRegex = /^\/auth\/(login|signup)$|^\/product\/(\d+)?$/;
+  const notUseTokenUrlRegex = /^\/auth\/(login|signup)$|^\/product.*$/;
 
   return notUseTokenUrlRegex.test(url);
 };
