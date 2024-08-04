1cb2abf289cd15aac58e20cf870dae0da24b69f8
pki071120 <ddametyy@gmail.com>
2024-08-04 22:59:00 +0900

    Add QueryKeys

--- packages/shared/src/libs/apis/querykeys.ts
+++ packages/shared/src/libs/apis/querykeys.ts
@@ -2,3 +2,10 @@ export const authQueryKeys = {
   postLogin: () => ['auth', 'login', 'post'],
   getVerify: () => ['get', 'verify', 'token'],
 } as const;
+
+export const addressQueryKeys = {
+  getAddress: () => ['get', 'address', 'token'],
+  postAddress: () => ['post', 'address', 'token'],
+  putAddress: (id: number) => ['put', `address/${id}`, 'token'],
+  deleteAddress: (id: number) => ['delete', `address/${id}`, 'token'],
+}
