d57a3af6581f2dafef7296530b38bf0e051fe67a
pki071120 <ddametyy@gmail.com>
2024-08-04 22:58:18 +0900

    Add api

---
+++ packages/shared/src/hooks/apis/address/useGetAddress.ts
@@ -0,0 +1,14 @@
+import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import { addressQueryKeys, addressUrl, get } from 'shared/libs';
+import { AddressDataType } from 'shared/types';
+
+export const useGetAddress = (
+  options?: Omit<UseQueryOptions<AddressDataType[] | null, AxiosError>, 'queryKey'>
+) =>
+  useQuery({
+    queryKey: addressQueryKeys.getAddress(),
+    queryFn: () =>
+      get<AddressDataType[]>(addressUrl.getAddress()),
+    ...options,
+  });
