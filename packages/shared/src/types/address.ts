a286030df5f89438abb4fbf526f2c0359c9998ac
pki071120 <ddametyy@gmail.com>
2024-08-04 22:58:37 +0900

    Add type

---
+++ packages/shared/src/types/address.ts
@@ -0,0 +1,10 @@
+export interface AddressDataType {
+  id: number,
+  tag: string,
+  zipcode: number,
+  address: string,
+  detail: string,
+  request: string,
+  phone: string,
+  user: number
+}
