fc45dfc9604426ae13b30f2d702b041173f384cf
gaoooon <gaon9749@gmail.com>
2024-08-08 01:50:49 +0900

    Add CartType

---
+++ packages/shared/src/types/cart.ts
@@ -0,0 +1,13 @@
+export interface PostCartType {
+  product: number;
+  count: number;
+  option: number;
+}
+
+export interface CartProductType {
+  id: number;
+  count: number;
+  user: number;
+  product: number;
+  option: number;
+}
