1aa4adb053eb1f629d0d12355c88b5b3c71727f1
gaoooon <gaon9749@gmail.com>
2024-08-16 02:47:51 +0900

    Update GlobalStyle

--- packages/shared/src/styles/GlobalStyle.tsx
+++ packages/shared/src/styles/GlobalStyle.tsx
@@ -39,33 +39,6 @@ const GlobalStyle = () => (
         color: inherit;
       }
 
-      html {
-        @media (max-width: 599px) {
-          font-size: 15px;
-        }
-        @media (max-width: 530px) {
-          font-size: 14px;
-        }
-        @media (max-width: 490px) {
-          font-size: 13px;
-        }
-        @media (max-width: 460px) {
-          font-size: 12px;
-        }
-        @media (max-width: 420px) {
-          font-size: 11px;
-        }
-        @media (max-width: 390px) {
-          font-size: 10px;
-        }
-        @media (max-width: 350px) {
-          font-size: 9px;
-        }
-        @media (max-width: 320px) {
-          font-size: 8px;
-        }
-      }
-
       button {
         padding: 0;
         border: 0;
