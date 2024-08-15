f1cb244f7d645e241b2332bb1cdb34ab7657bf73
gaoooon <gaon9749@gmail.com>
2024-08-16 02:48:10 +0900

    Delete post login

--- packages/shared/src/libs/apis/querykeys.ts
+++ packages/shared/src/libs/apis/querykeys.ts
@@ -1,7 +1,6 @@
 import { BrandType } from 'shared/types';
 
 export const authQueryKeys = {
-  postLogin: () => ['auth', 'login', 'post'],
   getVerify: () => ['get', 'verify', 'token'],
 } as const;
 
