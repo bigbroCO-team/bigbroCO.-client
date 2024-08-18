b6220068f22bdfbf27a120e5990a96d6a21539a7
gaoooon <gaon9749@gmail.com>
2024-08-19 02:16:44 +0900

    Add mediaquery

--- apps/client/src/components/ProductItem/style.ts
+++ apps/client/src/components/ProductItem/style.ts
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import Link from 'next/link';
+import { MobileWidth } from 'shared';
 
 export const Wrapper = styled(Link)`
   display: flex;
@@ -7,12 +8,21 @@ export const Wrapper = styled(Link)`
   justify-content: center;
   align-items: center;
   gap: 0.75rem;
+
+  @media (max-width: ${MobileWidth}px) {
+    gap: 0.5rem;
+  }
 `;
 
 export const ImgContainer = styled.img`
   width: 24.75rem;
   height: 24.75rem;
   object-fit: cover;
+
+  @media (max-width: ${MobileWidth}px) {
+    width: 9.625rem;
+    height: 9.625rem;
+  }
 `;
 
 export const PriceInform = styled.div`
@@ -23,16 +33,28 @@ export const PriceInform = styled.div`
 export const DiscountText = styled.p`
   ${({ theme }) => theme.typo.title.small};
   color: ${({ theme }) => theme.color.error};
+
+  @media (max-width: ${MobileWidth}px) {
+    ${({ theme }) => theme.typo.body.medium};
+  }
 `;
 
 export const PriceText = styled.p`
   ${({ theme }) => theme.typo.title.small};
   color: ${({ theme }) => theme.color.white};
+
+  @media (max-width: ${MobileWidth}px) {
+    ${({ theme }) => theme.typo.body.small};
+  }
 `;
 
-export const ClothesText = styled.p`
+export const ProductName = styled.p`
   ${({ theme }) => theme.typo.body.small};
   color: ${({ theme }) => theme.color.white};
+
+  @media (max-width: ${MobileWidth}px) {
+    ${({ theme }) => theme.typo.label};
+  }
 `;
 
 export const EditText = styled.button`
