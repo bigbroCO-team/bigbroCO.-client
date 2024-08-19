4adf40d37aeb43a53d12f4aa5c5ecff296c798b5
pki071120 <ddametyy@gmail.com>
2024-08-19 10:43:01 +0900

    Update LayoutIndex

--- apps/client/src/layouts/index.ts
+++ apps/client/src/layouts/index.ts
@@ -2,3 +2,4 @@ export { default as LoginLayout } from './login';
 export { default as MainLayout } from './main';
 export { default as ProductListLayout } from './productList';
 export { default as ProductLayout } from './product';
+export { default as MyPageLayout } from './myPage';
