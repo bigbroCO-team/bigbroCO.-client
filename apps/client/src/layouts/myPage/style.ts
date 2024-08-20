5298a4dc83202ec5ddea693e01aaf86f50c8d33c
pki071120 <ddametyy@gmail.com>
2024-08-20 19:59:43 +0900

    Update MyPageLayout

--- apps/client/src/layouts/myPage/style.ts
+++ apps/client/src/layouts/myPage/style.ts
@@ -1,29 +1,49 @@
 import styled from '@emotion/styled';
 import Link from 'next/link';
+import { MobileWidth } from 'shared';
 
 export const Container = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
   height: auto;
-  padding-top: 116px;
+  justify-content: center;
+  padding-top: 7.25rem;
   background-color: ${({ theme }) => theme.color.black};
+
+  @media (max-width: ${MobileWidth}px) {
+    padding-top: 4.6875rem;
+  }
+`;
+
+export const Wrapper = styled.div`
+  width: 55%;
+  height: auto;
+
+  @media (max-width: ${MobileWidth}px) {
+    width: 100%;
+    padding: 1rem;
+  }
 `;
 
 export const TitleWrapper = styled.div`
   display: flex;
-  width: 56.25rem;
+  width: auto;
   align-items: flex-end;
   justify-content: space-between;
-  margin-bottom: 60px;
+  margin-bottom: 3.75rem;
+
+  @media (max-width: ${MobileWidth}px) {
+    margin-bottom: 1.5rem;
+  }
 `;
 
-export const UserName = styled.div`
-  ${({ theme }) => theme.typo.title.medium};
+export const UserName = styled.div<{ isMobile: boolean }>`
+  ${({ theme, isMobile }) =>
+    isMobile ? theme.typo.body.large : theme.typo.title.medium};
   color: ${({ theme }) => theme.color.white};
 
   span {
-    ${({ theme }) => theme.typo.title.small};
+    ${({ theme, isMobile }) =>
+      isMobile ? theme.typo.body.large : theme.typo.title.small};
   }
 `;
 
@@ -35,13 +55,13 @@ export const ModifyInfo = styled(Link)`
 export const ContentsWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  width: 56.25rem;
-  gap: 24px;
+  width: auto;
+  gap: 1.5rem;
   margin-bottom: 27px;
 `;
 
 export const Hr = styled.hr`
-  width: 900px;
-  height: 1px;
+  width: 100%;
+  height: 0.0625rem;
   border-color: ${({ theme }) => theme.color.grey[800]};
 `;
